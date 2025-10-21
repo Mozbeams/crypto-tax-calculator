92a7ac7e783d7c26a4143e2233d44e16474a22bd
Noah <102112105+s4myot@users.noreply.github.com>
2025-10-21 20:37:34 +0200

    UIE update

--- src/conversion/exchanges.py
+++ src/conversion/exchanges.py
@@ -1,5 +1,5 @@
 # Exchange-specific Notes
-
+ 
 This page combines common gotchas and Information which are exchange-specific and most likely don't apply to other exchanges.
 
 ## Exchange configuration
